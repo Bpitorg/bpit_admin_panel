87318721b884c26a769ed32ebbecfdfed2a9e358
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-03-04 10:05:59 +0530

    Update api_endpoints.js & appbar.js

--- src/components/navbar/appbar.js
+++ src/components/navbar/appbar.js
@@ -1,38 +1,36 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import MenuIcon from '@mui/icons-material/Menu';
-import MuiAppBar from '@mui/material/AppBar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Logout from '@mui/icons-material/Logout';
-import SettingsIcon from '@mui/icons-material/Settings';
-import { grey } from '@mui/material/colors';
-import { useNavigate } from 'react-router-dom';
-// import Brightness4Icon from '@mui/icons-material/Brightness4';
-// import Brightness7Icon from '@mui/icons-material/Brightness7';
-
-// const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import MuiAppBar from "@mui/material/AppBar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Tooltip from "@mui/material/Tooltip";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Logout from "@mui/icons-material/Logout";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { grey } from "@mui/material/colors";
+import { useNavigate } from "react-router-dom";
+import http_lib from "../../http/http";
+import { LOGOUT_URL } from "../../constants/api_endpoints";
 
 const drawerWidth = 240;
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -53,17 +51,22 @@ export default function Bar(props) {
   }
 
   function handleLogout() {
-    localStorage.clear();
-    navigate('');
+    http_lib
+      .get(LOGOUT_URL)
+      .then((res) => {
+        localStorage.clear();
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
   }
-  // const theme = useTheme();
-  // const colorMode = React.useContext(ColorModeContext);
-
+  
   return (
     <AppBar position="absolute" open={props.open}>
       <Toolbar
         sx={{
-          pr: '24px', // keep right padding when drawer closed
+          pr: "24px", // keep right padding when drawer closed
         }}
       >
         <IconButton
@@ -72,8 +75,8 @@ export default function Bar(props) {
           aria-label="open drawer"
           onClick={props.toggleDrawer}
           sx={{
-            marginRight: '36px',
-            ...(props.open && { display: 'none' }),
+            marginRight: "36px",
+            ...(props.open && { display: "none" }),
           }}
         >
           <MenuIcon />
@@ -92,16 +95,14 @@ export default function Bar(props) {
             onClick={handleClick}
             size="small"
             sx={{ ml: 2 }}
-            aria-controls={open ? 'account-menu' : undefined}
+            aria-controls={open ? "account-menu" : undefined}
             aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
+            aria-expanded={open ? "true" : undefined}
           >
             <SettingsIcon sx={{ color: grey[50] }} />
           </IconButton>
         </Tooltip>
-        {/* <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-          {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-        </IconButton> */}
+        
       </Toolbar>
       <Menu
         anchorEl={anchorEl}
@@ -112,31 +113,31 @@ export default function Bar(props) {
         PaperProps={{
           elevation: 0,
           sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
             mt: 1.5,
-            '& .MuiAvatar-root': {
+            "& .MuiAvatar-root": {
               width: 32,
               height: 32,
               ml: -0.5,
               mr: 1,
             },
-            '&:before': {
+            "&:before": {
               content: '""',
-              display: 'block',
-              position: 'absolute',
+              display: "block",
+              position: "absolute",
               top: 0,
               right: 14,
               width: 10,
               height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
               zIndex: 0,
             },
           },
         }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
         {/* <MenuItem>
           <ListItemIcon>
@@ -152,5 +153,5 @@ export default function Bar(props) {
         </MenuItem>
       </Menu>
     </AppBar>
-  )
+  );
 }
